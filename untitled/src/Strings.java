837b002dc8669d24537d3c5988b84c640e9b8310
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2025-03-14 19:53:10 -0300

    Update Strings.java

--- untitled/src/Strings.java
+++ untitled/src/Strings.java
@@ -3,7 +3,7 @@ public class Strings {
         char vetor[] = {'o', 'l', 'a'};
         String texto = "ola";
         for(int i=0; i<texto.length(); i++){
-            System.out.println(texto.carAt(i));
+            System.out.println(texto.charAt(i));
         }
         texto.replace("o", "x");
         texto.toLowerCase();
